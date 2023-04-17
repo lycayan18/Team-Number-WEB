3dc4ad270d53f5cf0d6023e1450955809c7f8c2e
Egor <8644233@gmail.com>
2023-04-17 22:41:33 +0300

    Add fucn add img

--- forms/admin.py
+++ forms/admin.py
@@ -7,10 +7,3 @@ class EditingForm(FlaskForm):
     title = StringField('Заголовок', validators=[DataRequired()])
     content = TextAreaField("Содержание")
     submit = SubmitField('Применить')
-
-
-class AddForms(FlaskForm):
-    title = StringField('Заголовок', validators=[DataRequired()])
-    content = TextAreaField('Содержание')
-    img = FileField('Загрузить изображение')
-    submit = SubmitField('Добавить')
