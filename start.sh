1424f439f5e8418960d8a0f854b2726d146cdd7d
Egor <8644233@gmail.com>
2023-04-19 23:02:46 +0300

    Fixed start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@
 set -eu
 
 # Python buffers stdout. Without this, you won't see what you "print" in the Activity Logs
+export PYTHONUNBUFFERED=true
 
 # Install Python 3 virtual env
 VIRTUALENV=./venv
